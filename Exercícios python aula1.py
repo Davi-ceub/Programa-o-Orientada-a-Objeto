6cfcaefc89738cc59d06e7cc0762bf8cceceaa78
Davi Castilho <davi.lc@sempreceub.com>
2025-04-07 16:08:24 -0300

    Add files via upload

---
+++ Exercícios python aula1.py
@@ -0,0 +1,30 @@
+#1)
+# a)
+x = 5
+y = 10
+print((x * y)/2)
+
+#b) 
+x = int(input("Digite a base: "))
+y = int(input("Digite a altura: "))
+print((x * y)/2)
+
+#2)
+nome = input("Informe o seu nome: ")
+print(nome)
+
+#3)
+dinheiro = float(input("informe a quantidade: "))
+print(dinheiro/6)
+print(dinheiro/6.5)
+
+#4)
+altura = float(input("Digite a altura: "))
+peso = float(input("Digite o peso: "))
+print(peso/(altura * altura))
+
+#Extra
+sálario = int(input("Digite o sálario: "))
+fgts = (sálario * 0.08)
+print(fgts)
+print(sálario - fgts)
